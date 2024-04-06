5169fb7903b2b2e9464a986195976ebbf2d003b4
reem <tarekreem950@gmail.com>
2024-04-06 18:03:54 +0200

    Deletion-resilient hypermedia pagination Deletion-resilient hypermedia pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -42,4 +42,22 @@ class Server:
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Deletion-resilient hypermedia pagination"""
-        pass
+        indexes = list(self.__indexed_dataset.keys())
+        assert index is not None and index <= int(indexes[-1])
+        ran = index + page_size
+        count = index
+        data = []
+        while len(data) < page_size:
+            if (count > indexes[-1]):
+                break
+            if (count in indexes):
+                data.append(self.__indexed_dataset[count])
+            count = count + 1
+
+        res = {
+            'index': index,
+            'data': data,
+            'page_size': page_size,
+            'next_index': ran
+        }
+        return res
