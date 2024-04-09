a9218ae3006b9c62d20c3b8cdb720a66ed5ba9a8
reem <tarekreem950@gmail.com>
2024-04-09 21:52:01 +0200

    pycode

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,8 +3,6 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-
 
 class Config():
     """configure class"""
@@ -13,6 +11,7 @@ class Config():
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
