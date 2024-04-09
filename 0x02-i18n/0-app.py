13f9ce58264adc5ae633df96fab13676fbac55dc
reem <tarekreem950@gmail.com>
2024-04-09 20:53:40 +0200

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,34 +1,15 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
-# from flask import Flask, render_template
-
-
-# app = Flask(__name__)
-
-
-# @app.route("/", strict_slashes=False)
-# def route():
-#     """route func"""
-#     return render_template('0-index.html')
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-    
-    
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Renders a basic html template
-    """
+@app.route("/")
+def route():
+    """route func"""
     return render_template('0-index.html')
 
 
