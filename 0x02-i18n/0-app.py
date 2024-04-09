9dbf1676fee38ba74e0f5e18c6cd959e73f6d9de
reem <tarekreem950@gmail.com>
2024-04-09 20:48:55 +0200

    task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def route():
     """route func"""
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
