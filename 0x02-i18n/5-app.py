612545557869d43080edd679b07957d133cecf85
reem <tarekreem950@gmail.com>
2024-04-09 23:24:08 +0200

    file for tasks

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config():
+    """configure class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """route func"""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
