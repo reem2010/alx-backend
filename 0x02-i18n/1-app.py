0f2dc861ab13c7b93e68862650d0cb42434c3cb6
reem <tarekreem950@gmail.com>
2024-04-10 15:13:53 +0200

    class attributes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,8 +6,8 @@ from flask_babel import Babel
 
 class Config():
     """configure class"""
-    LANGUAGES = ["en", "fr"],
-    BABEL_DEFAULT_LOCALE = "en",
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
